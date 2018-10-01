f3e7868c9835805fa9b5f14206802b0dcb7191b9
Alexsis <mymail@the.best>
2018-10-01 17:01:18 +0300

    Completed Stickey header func

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 let mobileMenu = new MobileMenu();
 // var revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonials"), "60%");
+let stickyHeader = new StickyHeader();
